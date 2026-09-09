2a93713772ec5afe9674dd05d455732c3f07e1cc
YourNelvi <nelvisgabrielr@gmail.com>
2026-09-09 14:04:31 -0400

    chore: bump version to 1.14.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.erp"
         minSdk = 30
         targetSdk = 37
-        versionCode = 1013000
-        versionName = "1.13.0"
+        versionCode = 1014000
+        versionName = "1.14.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
